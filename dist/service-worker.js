352dd009a8f9a4c4ec8c253ca04c48b269ec4580
Roriz <radamesroriz@gmail.com>
2018-12-18 17:04:27 -0200

    ci(package.json): update version to 0.3.0

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.224a6e6e685010d1db602e454bd79b7a.js"
+  "precache-manifest.c4a22fc749cd1ad7e96fc18937c628dd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-forms"});
